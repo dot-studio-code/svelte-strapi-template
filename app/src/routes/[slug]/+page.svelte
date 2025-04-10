cfeb27d6c671693d16adf7fefb3537099ae9bcc2
Tilman M <mail@tlmn.io>
2025-04-10 09:28:30 +0200

    add SEO data

--- app/src/routes/[slug]/+page.svelte
+++ app/src/routes/[slug]/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	import BlockSwitch from '$lib/ui/BlockSwitch.svelte';
 	import type { PageData } from './$types';
+	import { PUBLIC_BASE_URL } from '$env/static/public';
 
 	let { data }: { data: PageData } = $props();
+	const pageData = $derived(data.pages[0]);
 </script>
 
-<BlockSwitch page={data.data.pages[0]} />
+<svelte:head>
+	<title>{data.data.siteSetting?.siteTitle} – {pageData?.SEO.pageTitle}</title>
+	<meta name="description" content={pageData?.SEO.pageDescription} />
+	<meta name="author" content={data.data.siteSetting?.author} />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="keywords" content={data.data.siteSetting?.keywords} />
+	<meta property="og:title" content={pageData?.SEO.pageTitle} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={`${PUBLIC_BASE_URL}${page.url}`} />
+	<meta property="og:image" content={pageData?.SEO.ogImage.url} />
+</svelte:head>
+
+<BlockSwitch page={pageData} />
